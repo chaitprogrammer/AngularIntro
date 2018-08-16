3ec8e4a7c12450711c7c4cb93402ed9bb466d28c
Chaitanya <chait.connect@gmail.com>
2018-08-16 00:06:16 -0400

    Angular components menu grid view with selected item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
@@ -24,7 +29,10 @@ import { MenuComponent } from './menu/menu.component';
     HttpModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
